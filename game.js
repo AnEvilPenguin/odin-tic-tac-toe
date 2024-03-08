5425ad149acb2a71d9bef0fa7bcb134be75876b0
AnEvilPenguin <dave.lewis@squaredup.com>
2024-03-08 11:20:44 +0000

    feat: check win conditions

--- game.js
+++ game.js
@@ -43,16 +43,60 @@ function makeGameController() {
     const setNextPlayer = () => currentPlayer === player1 ?
         currentPlayer = player2 :
         currentPlayer = player1;
+    
+    const checkArray = (arr) => {
+        if (!Array.isArray(arr)) {
+            throw new Error('Must provide valid array');
+        }
+
+        const unique = Array.from(new Set(arr));
+        return unique.length === 1 && unique[0] != null;
+    }
+
+    const getDiagonal1 = () => [board[0][0], board[1][1], board[2][2]];
+    const getDiagonal2 = () => [board[0][2], board[1][1], board[2][0]];
+
+    const checkWin = (x, y) => {
+
+        const rowWin = checkArray(board[y]);
+        const columnWin = checkArray(board.map(r => r[x]));
+        let diag1Win;
+        let diag2Win;
+
+        if (x - y === 0) {
+            diag1Win = checkArray(getDiagonal1());
+        }
+
+        if (x + y === 2) {
+            diag2Win = checkArray(getDiagonal2());
+        }
+
+        return {
+            rowWin,
+            columnWin,
+            diag1Win,
+            diag2Win,
+            win: rowWin || columnWin || diag1Win || diag2Win,
+        };
+    }
 
     const makePlay = (x, y) => {
         playToken(x, y, currentPlayer.token);
-        // TODO check if player won
+        const winDetails = checkWin(x, y);
+
+        if (winDetails.win) {
+            return {
+                ...currentPlayer,
+                ...winDetails,
+            };
+        }
+        
         setNextPlayer();
     };
 
     return {
         board,
         makePlay,
+        checkWin,
     };
 }
-
