c13e1b95ebc43394889e24f51eab5805cbcb1b6a
AnEvilPenguin <17614766+AnEvilPenguin@users.noreply.github.com>
2024-03-11 22:02:42 +0000

    feat: restart game and module pattern for controller

--- display.js
+++ display.js
@@ -1,60 +1,86 @@
 
-let controller = makeGameController();
+(() => {
+    const button = document.querySelector(".status button");
+    console.log(button);
 
-const board = document.querySelector('.board');
-const statusText = document.querySelector('.status p');
+    const startGame = () => {
+        const controller = makeGameController();
 
-const remainingSpaces = [];
-let id = 0;
+        const board = document.querySelector(".board");
+        const statusText = document.querySelector(".status p");
 
-for (let y = 0; y < 3; y++) {
-    for (let x = 0; x < 3; x++) {
-        const space = document.createElement('div');
-        space.classList.add('space');
-        space.id = id++;
+        statusText.textContent = "Player 1s Turn"; 
 
-        let onClick;
+        // clears all child nodes
+        board.textContent = "";
 
-        onClick = () => {
-            space.removeEventListener("click", onClick);
+        const remainingSpaces = [];
+        let id = 0;
 
-            remainingSpaces.splice(remainingSpaces.findIndex(s => s.id === space.id), 1);
+        const buttonNewGame = () => button.textContent = "New Game";
+        button.textContent = "Restart Game";
 
-            controller.makePlay(x, y);
+        for (let y = 0; y < 3; y++) {
+            for (let x = 0; x < 3; x++) {
+                const space = document.createElement("div");
+                space.classList.add("space");
+                space.id = id++;
 
-            const token = controller.getSpace(x, y);
+                let onClick;
 
-            space.textContent = token === 1 ? "X" : "O";
+                onClick = () => {
+                    space.removeEventListener("click", onClick);
 
-            if (controller.isWin(x, y)) {
-                console.log("Winner");
-                
-                statusText.textContent = token === 1 ?
-                    "Player 1 wins!" : "Player 2 wins!";
+                    remainingSpaces.splice(remainingSpaces.findIndex(s => s.id === space.id), 1);
 
-                remainingSpaces.forEach(s => s.clearOnClick());
-                return;
+                    controller.makePlay(x, y);
+
+                    const token = controller.getSpace(x, y);
+
+                    space.textContent = token === 1 ? "X" : "O";
+
+                    if (controller.isWin(x, y)) {
+                        console.log("Winner");
+
+                        statusText.textContent = token === 1 ?
+                            "Player 1 wins!" : "Player 2 wins!";
+
+                        remainingSpaces.forEach(s => s.clearOnClick());
+
+                        buttonNewGame();
+                        return;
+                    }
+
+                    if (controller.isTie()) {
+                        console.log("Tie game");
+
+                        statusText.textContent = "Tie game";
+
+                        button.textContent = "New Game";
+
+                        buttonNewGame();
+                        return;
+                    }
+
+                    statusText.textContent = token === 1 ?
+                        "Player 2s turn" : "Player 1s turn";
+                };
+
+                space.addEventListener("click", onClick);
+
+                remainingSpaces.push({
+                    id: space.id,
+                    clearOnClick: () => space.removeEventListener("click", onClick),
+                });
+
+                board.appendChild(space);
             }
-
-            if (controller.isTie()) {
-                console.log("Tie game");
-
-                statusText.textContent = "Tie game";
-                return;
-            }
-
-            statusText.textContent = token === 1 ? 
-                "Player 2s turn" : "Player 1s turn";
-        };
-
-        space.addEventListener("click", onClick);
-
-        remainingSpaces.push({
-            id: space.id,
-            clearOnClick: () => space.removeEventListener("click", onClick),
-        });
-
-        board.appendChild(space);
+        }
     }
-}
 
+    button.addEventListener("click", () => {
+        console.log("clicked");
+        startGame();
+    });
+    startGame();
+})();
