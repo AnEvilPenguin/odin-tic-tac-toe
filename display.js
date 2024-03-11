92333806638f16e29b80f4e1885af7fd8bfee6f3
AnEvilPenguin <17614766+AnEvilPenguin@users.noreply.github.com>
2024-03-11 20:11:49 +0000

    feat: better handling

--- display.js
+++ display.js
@@ -1,28 +1,46 @@
 
 let controller = makeGameController();
 
-let board = document.querySelector('.board');
+const board = document.querySelector('.board');
+
+const remainingSpaces = [];
+let id = 0;
 
 for (let y = 0; y < 3; y++) {
     for (let x = 0; x < 3; x++) {
         const space = document.createElement('div');
         space.classList.add('space');
+        space.id = id++;
 
         let onClick;
 
         onClick = () => {
-            const result = controller.makePlay(x, y);
-            console.log(result);
+            space.removeEventListener("click", onClick);
+
+            remainingSpaces.splice(remainingSpaces.findIndex(s => s.id === space.id), 1);
+
+            controller.makePlay(x, y);
 
             const token = controller.getSpace(x, y);
-            console.log(token);
-
             space.textContent = token === 1 ? 'X' : 'O';
 
-            space.removeEventListener("click", onClick);
+            if (controller.isWin(x, y)) {
+                console.log('Winner');
+                remainingSpaces.forEach(s => s.clearOnClick());
+            }
+
+            if (controller.isTie()) {
+                console.log('Tie game');
+            }
         };
 
         space.addEventListener("click", onClick);
+
+        remainingSpaces.push({
+            id: space.id,
+            clearOnClick: () => space.removeEventListener("click", onClick),
+        });
+
         board.appendChild(space);
     }
 }
