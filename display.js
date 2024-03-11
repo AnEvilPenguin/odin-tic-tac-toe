d0faafb6b64b9be2b1956a9eb40eb5cd1a311ad2
AnEvilPenguin <17614766+AnEvilPenguin@users.noreply.github.com>
2024-03-11 18:29:39 +0000

    feat: dynamic board creation

--- display.js
+++ display.js
@@ -1,2 +1,11 @@
 
 let controller = makeGameController();
+
+let board = document.querySelector('.board');
+
+for (let i = 0; i < 9; i++) {
+    const space = document.createElement('div');
+    space.classList.add('space');
+
+    board.appendChild(space);
+}
