c71b3f369377c1bffb52506c7c72a1ecceaa8e8b
AnEvilPenguin <17614766+AnEvilPenguin@users.noreply.github.com>
2024-03-11 19:11:42 +0000

    feat: basic game interface

--- display.js
+++ display.js
@@ -3,9 +3,26 @@ let controller = makeGameController();
 
 let board = document.querySelector('.board');
 
-for (let i = 0; i < 9; i++) {
-    const space = document.createElement('div');
-    space.classList.add('space');
+for (let y = 0; y < 3; y++) {
+    for (let x = 0; x < 3; x++) {
+        const space = document.createElement('div');
+        space.classList.add('space');
 
-    board.appendChild(space);
+        let onClick;
+
+        onClick = () => {
+            const result = controller.makePlay(x, y);
+            console.log(result);
+
+            const token = controller.board[y][x];
+            // TODO replace this with function...
+            console.log(token);
+
+            space.removeEventListener("click", onClick);
+        };
+
+        space.addEventListener("click", onClick);
+        board.appendChild(space);
+    }
 }
+
