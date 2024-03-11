b290516e8970ca3cb4996bf648a2269e157a83bf
AnEvilPenguin <17614766+AnEvilPenguin@users.noreply.github.com>
2024-03-11 20:20:30 +0000

    feat: handle win conditions in status text

--- display.js
+++ display.js
@@ -2,6 +2,7 @@
 let controller = makeGameController();
 
 const board = document.querySelector('.board');
+const statusText = document.querySelector('.status p');
 
 const remainingSpaces = [];
 let id = 0;
@@ -22,16 +23,28 @@ for (let y = 0; y < 3; y++) {
             controller.makePlay(x, y);
 
             const token = controller.getSpace(x, y);
-            space.textContent = token === 1 ? 'X' : 'O';
+
+            space.textContent = token === 1 ? "X" : "O";
 
             if (controller.isWin(x, y)) {
-                console.log('Winner');
+                console.log("Winner");
+                
+                statusText.textContent = token === 1 ?
+                    "Player 1 wins!" : "Player 2 wins!";
+
                 remainingSpaces.forEach(s => s.clearOnClick());
+                return;
             }
 
             if (controller.isTie()) {
-                console.log('Tie game');
+                console.log("Tie game");
+
+                statusText.textContent = "Tie game";
+                return;
             }
+
+            statusText.textContent = token === 1 ? 
+                "Player 2s turn" : "Player 1s turn";
         };
 
         space.addEventListener("click", onClick);
